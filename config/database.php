ff6b9b29229834bbdcac663b37e3d95eabe9b38f
KamauAdrian <35832303+KamauAdrian@users.noreply.github.com>
2019-03-26 11:34:44 +0300

    added mysql db to laravel app in heroku

--- config/database.php
+++ config/database.php
@@ -1,4 +1,10 @@
 <?php
+$url = parse_url(getenv("DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = Substr($url["path"],1);
+
 
 return [
 
@@ -13,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' =>'pgsql',
 
     /*
     |--------------------------------------------------------------------------
